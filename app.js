7d299fcdc5d55a4df50488343542b4d1451a2f52
umcsoagito <69760926+umcsoagito@users.noreply.github.com>
2020-08-16 15:46:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBkowB9f1NWBPCkHrn-rFUf7CZlj3NMBqg",
+  authDomain: "hacking-721b6.firebaseapp.com",
+  databaseURL: "https://hacking-721b6.firebaseio.com",
+  projectId: "hacking-721b6",
+  storageBucket: "hacking-721b6.appspot.com",
+  messagingSenderId: "612531937566",
+  appId: "1:612531937566:web:9f074fc2bc1f0d7b3be90f",
+  measurementId: "G-8HVNCTLRW2"
 };
 
      
